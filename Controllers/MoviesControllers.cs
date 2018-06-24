63b36d348ac690a024f5e568b80d5ffe61bbd20d
Katelynn Campbell <klyncamp23@gmail.com>
2018-06-24 13:01:04 -0400

    MVC

---
+++ Controllers/MoviesControllers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NetMVC.Models;
+
+namespace ASP.NetMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET : Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Sherk!" };
+            return View(movie);
+        }
+    }
+}
